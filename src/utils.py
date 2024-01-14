92e31b3e37f319881f36d9ba0b269d26cdbab30b
Aniol <aniol0012@gmail.com>
2024-01-14 16:52:25 +0100

    fix: plot print

--- src/utils.py
+++ src/utils.py
@@ -26,21 +26,28 @@ def print_parameter_usage(args):
 
 def plot_clusters(items, assignments, clusters, iteration):
     colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+    clusters_items = []
 
     for i, cluster in enumerate(clusters):
         cluster_items = [items[j] for j, a in enumerate(assignments) if a == i]
+        clusters_items.append(cluster_items)
 
-        x_items, y_items = zip(*cluster_items)
-        plt.scatter(x_items, y_items, c=colors[i % len(colors)], marker='o', label=f'Cluster {i + 1}')
+    if clusters_items:
+        for i, cluster_items in enumerate(clusters_items):
+            if cluster_items:
+                x_items, y_items = zip(*cluster_items)
+                plt.scatter(x_items, y_items, c=colors[i % len(colors)], marker='o', label=f'Cluster {i + 1}')
 
-        plt.scatter(cluster[0], cluster[1], c=colors[i % len(colors)], marker='x', s=100)
+                plt.scatter(clusters[i][0], clusters[i][1], c=colors[i % len(colors)], marker='x', s=100)
 
-    plt.title(f'Cluster Plot iteration {iteration}')
-    plt.xlabel('X Coordinate')
-    plt.ylabel('Y Coordinate')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+        plt.title(f'Cluster Plot iteration {iteration}')
+        plt.xlabel('X Coordinate')
+        plt.ylabel('Y Coordinate')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+    else:
+        print("No clusters to plot")
 
 
 def add_txt_extension(file):
@@ -58,11 +65,11 @@ def generate_random_file(file):
         file = '../data/' + file
 
     with open(file, 'w') as f:
-        f.write('#items\n')
+        f.write('# items\n')
         for i in range(items):
             f.write(f'{np.random.randint(0, 100)}, {np.random.randint(0, 100)}\n')
-        f.write('\n#clusters\n')
+        f.write('\n# clusters\n')
         for i in range(clusters):
             f.write(f'{np.random.randint(0, 100)}, {np.random.randint(0, 100)}\n')
-        f.write('\n#iterations\n')
+        f.write('\n# iterations\n')
         f.write(f'{iterations}\n')
